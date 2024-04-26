bab492a65c8c389ef068f9db1474f2c1639ec809
David Poole <14233215+davidlpoole@users.noreply.github.com>
2024-04-26 18:38:50 +1200

    Remove conditional downloading

--- background.js
+++ background.js
@@ -1,24 +1,17 @@
-let downloading = false
 let downloadedUrls = []
 
 browser.webRequest.onBeforeRequest.addListener(
   function (details) {
-    if (downloading && details.url.startsWith('https://t4.bcbits.com/stream')) {
+    if (details.url.startsWith('https://t4.bcbits.com/stream')) {
       if (!downloadedUrls.includes(details.url)) {
         downloadedUrls.push(details.url)
-        browser.downloads.download({ url: details.url })
+        browser.downloads.download({
+          url: details.url,
+          filename: `test.mp3`,
+        })
       }
     }
   },
   { urls: ['<all_urls>'] },
   ['blocking']
 )
-
-browser.runtime.onMessage.addListener((message) => {
-  if (message === 'startDownload') {
-    downloading = true
-    downloadedUrls = []
-  } else if (message === 'stopDownload') {
-    downloading = false
-  }
-})
